a66cd2d6be749b0574b679594ce1c0a795170c8a
CarlosDCN <48950589+CarlosCardona953@users.noreply.github.com>
2019-05-09 01:48:35 -0500

    Ejercicio 30

--- CarlosCardona_Ejercicio30.cpp
+++ CarlosCardona_Ejercicio30.cpp
@@ -29,8 +29,7 @@ int main(){
         
     for (int j=0;j<t;j++){
         for(int i=1;i<x-1;i++){
-           //u_new[j]=(1-beta*beta)*u[j]-(0.5*beta)*(1-beta)*u[j+1] + (0.5*beta)*(1 + beta )*u[j-1];
-            
+             
             
             u_new[i]=u[i]- (beta/4)*(pow(u[i+1],2)-pow(u[i-1],2)) +(pow(beta,2)/8)*( (u[i+1]+u[i])*(pow(u[i+1],2)-pow(u[i],2)) -(u[i]+u[i-1])*(pow(u[i],2)-pow(u[i-1],2)));
         
